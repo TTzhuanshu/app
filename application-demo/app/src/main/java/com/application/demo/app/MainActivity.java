c015bb9ad2bd7f4469fb13e3299112005bf6630a
TTzhuanshu <951421993@qq.com>
2018-04-10 14:20:56 +0800

    add
    app

---
+++ application-demo/app/src/main/java/com/application/demo/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.application.demo.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
